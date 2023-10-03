5e2cf546e8009a826a86e79d690f5400481ae412
Amitkumarjiii <145652490+Amitkumarjiii@users.noreply.github.com>
2023-10-03 20:56:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'mdiskshortner.link)
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'mdiskshortner.link')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'ce176148c955a472069674cebc9d421098f1db10')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
