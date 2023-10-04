5356d2a0f972ed40c2efa2579e196d72735396ca
Amitkumarjiii <145652490+Amitkumarjiii@users.noreply.github.com>
2023-10-04 05:54:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -121,9 +121,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://post-filter-bot-cntx.onrender.com/".format(FQDN)
+    URL = "https://post-filter-bot-jenx.onrender.com/".format(FQDN)
 else:
-    URL = "https://post-filter-bot-cntx.onrender.com/".format(FQDN)
+    URL = "https://post-filter-bot-jenx.onrender.com/".format(FQDN)
 
 
 
